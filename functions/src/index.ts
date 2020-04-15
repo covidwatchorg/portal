8ff5c9f05de243b8395747dc3d8ac50be3e65984
Madhava Jay <me@madhavajay.com>
2020-04-15 14:04:53 +1000

    Added tcn-node package and tested interface works
    - WIP waiting for some mode changes on the npm package side

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
-import * as admin from "firebase-admin"
-import * as functions from "firebase-functions"
-import { DateClass, Field, parse } from "sparkson"
+import * as admin from 'firebase-admin'
+import * as functions from 'firebase-functions'
+import {DateClass, Field, parse} from 'sparkson'
+import tcn from 'tcn-node'
 
 admin.initializeApp(functions.config().firebase)
 
@@ -8,40 +9,42 @@ export const firestore = admin.firestore()
 
 export class Report {
   constructor(
-    @Field("contact_event_key_bytes") public contact_event_key_bytes: string,
-    @Field("memo_data") public memo_data: string,
-    @Field("memo_type") public memo_type: number,
-    @Field("start_index") public start_index: number,
-    @Field("end_index") public end_index: number,
-    @Field("signature_bytes") public signature_bytes: string,
-    @Field("report_verification_public_key_bytes")
-    public report_verification_public_key_bytes: string,
-    @Field("timestamp") public timestamp: DateClass
-  ) {}
+      @Field('contact_event_key_bytes') public contact_event_key_bytes: string,
+      @Field('memo_data') public memo_data: string,
+      @Field('memo_type') public memo_type: number,
+      @Field('start_index') public start_index: number,
+      @Field('end_index') public end_index: number,
+      @Field('signature_bytes') public signature_bytes: string,
+      @Field('report_verification_public_key_bytes')
+      public report_verification_public_key_bytes: string,
+      @Field('timestamp') public timestamp: DateClass) {}
 }
 
 export const submitReport = functions.https.onRequest((request, response) => {
   console.log(request.body)
 
+  console.log(
+      'Call TCN Rust', tcn.tcn_example());  // should print "symptom data"
+
   try {
     // validate format
     const report = parse(Report, request.body)
-    console.log("Report JSON received: ", report)
+    console.log('Report JSON received: ', report)
 
     // validate crypto
 
     // prepare for storage
     const jsonObject = JSON.parse(JSON.stringify(report))
-    console.log("Saving report: ", jsonObject)
+    console.log('Saving report: ', jsonObject)
 
     firestore
-      .collection("signed_reports")
+      .collection('signed_reports')
       .doc()
       .set(jsonObject)
       .then(() => {
         return response.status(201).send({
-          status: "201",
-          message: "Success",
+          status: '201',
+          message: 'Success',
         })
       })
       .catch((error) => {
