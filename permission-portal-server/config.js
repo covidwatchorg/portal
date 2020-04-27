838e13d4dff2bc570d001b632f59f6ec23661b93
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2020-04-27 16:30:41 -0700

    changing config

--- permission-portal-server/config.js
+++ permission-portal-server/config.js
@@ -1,17 +1,19 @@
-import dotenv from 'dotenv';
-import {Pool} from 'pg';
+import dotenv from "dotenv";
+import { Pool } from "pg";
 
 dotenv.config();
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === "production";
 
 // const connectionString =
 //     `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${
 //         process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 const connectionString =
-    'postgres://iydpncmhthewfo:7cd68c7941af414c0644840790130cd88673c4c11087cc877cb80b43d28755cc@ec2-18-233-137-77.compute-1.amazonaws.com:5432/djppvnusl0spa';
+  "postgres://iydpncmhthewfo:7cd68c7941af414c0644840790130cd88673c4c11087cc877cb80b43d28755cc@ec2-18-233-137-77.compute-1.amazonaws.com:5432/djppvnusl0spa";
 
 export const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: true
+  connectionString: isProduction
+    ? process.env.HEROKU_POSTGRESQL_BLUE_URL
+    : connectionString,
+  ssl: true,
 });
