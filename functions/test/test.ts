ffd1da88c67556040949b7db51b957bb9838109a
martin <martin.bcu@gmail.com>
2020-04-29 03:50:00 +0900

    revamped test.ts on Firebase store, updated firestore.rules to allow write access for auth user and reject non-auth user

--- functions/test/test.ts
+++ functions/test/test.ts
@@ -1,7 +1,6 @@
 /// <reference path='../node_modules/mocha-typescript/globals.d.ts' />
 
-import { expect } from "chai"
-
+import { assert } from "chai"
 import * as firebase from "@firebase/testing"
 import { setup } from "./utils"
 
@@ -13,7 +12,9 @@ import { setup } from "./utils"
 
 const projectId = "tagstwo-431e3"
 const coverageUrl = `http://${process.env["FIRESTORE_EMULATOR_HOST"]}/emulator/v1/projects/${projectId}:ruleCoverage.html`
-
+const auth = {
+  uid: "covid"
+}
 // /*
 //  * ============
 //  *  Test Cases
@@ -26,54 +27,64 @@ beforeEach(async () => {
 })
 
 after(async () => {
+  // Tear down Firebase apps
   await Promise.all(firebase.apps().map((app) => app.delete()))
   console.log(`View rule coverage information at ${coverageUrl}\n`)
 })
 
-function makeSurveyData() {
-  return {
-    contact_event_key_bytes: "blah",
-    end_index: 21,
-    memo_data: "SGVsbG8sIF",
-    memo_type: 1,
-    report_verification_public_key_bytes: "oxChFfFyTH",
-    signature_bytes: "ETuzy1VioX",
-    start_index: 1,
-    timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-  }
+const mockData = {
+  contact_event_key_bytes: "blah",
+  end_index: 21,
+  memo_data: "SGVsbG8sIF",
+  memo_type: 1,
+  report_verification_public_key_bytes: "oxChFfFyTH",
+  signature_bytes: "ETuzy1VioX",
+  start_index: 1,
+  timestamp: firebase.firestore.FieldValue.serverTimestamp(),
 }
 
 @suite
 export class CovidWatchFirestore {
   @test
-  async "user can read signed_reports"() {
-    const mockData = {
-      signed_reports: {
-        alice: makeSurveyData(),
-      },
-    }
-    const db = await setup(projectId, null, mockData)
-    const report = db.collection("signed_reports").doc("alice")
-    await firebase.assertSucceeds(report.get())
-    await report
-      .get()
-      .then(function (doc: firebase.firestore.DocumentSnapshot) {
-        if (doc.exists) {
-          const data = doc.data()
-          if (data !== undefined && "contact_event_key_bytes" in data) {
-            return expect(data.contact_event_key_bytes).to.equal("blah")
-          }
-        }
-        return expect(false)
+  async "With firestore.rules, user with auth can read and write signed_reports"() {
+    const db = await setup(projectId, auth)
+    const report = db.collection("list").doc("item")
+
+    await firebase.assertSucceeds(
+      report.set({
+        some_random_message: "qwerty",
       })
+    )
+    await firebase.assertSucceeds(report.get())
   }
 
   @test
-  async "should not let anyone write a singed_reports"() {
+  async "Authenticated user should pass sanity check on write and update"() {
+    const db = await setup(projectId, auth)
+    const alice = db.collection("signed_reports").doc("alice");
+
+    await firebase.assertSucceeds(
+      alice.set(mockData)
+    )
+    let aliceData = await firebase.assertSucceeds(alice.get())
+    assert.equal(aliceData.data()["contact_event_key_bytes"], "blah", "Contact event key matched")
+
+    await firebase.assertSucceeds(
+      alice.update({
+        contact_event_key_bytes: "qwerty",
+      })
+    )
+    aliceData = await firebase.assertSucceeds(alice.get())
+    assert.equal(aliceData.data()["contact_event_key_bytes"], "qwerty", "Contact event key updated")
+  }
+
+  @test
+  async "With firestore.rules, user without auth cannot write a signed_reports"() {
     const db = await setup(projectId, null)
-    const survey = db.collection("signed_reports").doc("123")
+    const report = db.collection("signed_reports").doc("alice")
+    await firebase.assertSucceeds(report.get())
     await firebase.assertFails(
-      survey.set({
+      report.set({
         contact_event_key_bytes: "New Data",
       })
     )
--- functions/test/utils.ts
+++ functions/test/utils.ts
@@ -23,30 +23,11 @@ const setup = async (
   const app = await authedApp(projectId, auth)
   const db = app.firestore()
 
-  // Clear rules
-  // Here we load the insecure rules temporarily so we can write to the emulator
-  await firebase.loadFirestoreRules({
-    projectId,
-    rules: fs.readFileSync("firestore.insecure.rules", "utf8"),
-  })
-
-  // Write mock documents before rules
-  if (data) {
-    for (const collection in data) {
-      for (const doc in data[collection]) {
-        const ref = db.collection(collection).doc(doc)
-        await ref.set(data[collection][doc])
-      }
-    }
-  }
-
-  // Apply rules
-  // Restore the correct rules so we can test them
+  // Set Rules
   await firebase.loadFirestoreRules({
     projectId,
     rules: fs.readFileSync("../firestore.rules", "utf8"),
   })
-
   return db
 }
 
