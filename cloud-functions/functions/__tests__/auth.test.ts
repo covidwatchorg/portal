067ddf069876d07ec6ef1258d8d8bf88cf4353c7
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2020-05-02 11:38:47 -0700

    some of the tests weren't running the proper checks because of promise stuff

--- cloud-functions/functions/__tests__/auth.test.ts
+++ cloud-functions/functions/__tests__/auth.test.ts
@@ -4,6 +4,8 @@ import * as firebase from 'firebase/app';
 import 'firebase/auth';
 // tslint:disable-next-line: no-import-side-effect
 import 'firebase/functions';
+// tslint:disable-next-line: no-import-side-effect
+import 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAHVZXO-wFnGmUIBLxF6-mY3tuleK4ENVo',
@@ -17,7 +19,9 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-it('createUser cannot be called without being authenticated', () => {
+// const db = firebase.firestore();
+
+test('createUser cannot be called without being authenticated', () => {
   const createUser = firebase.functions().httpsCallable('createUser');
   return createUser({ email: 'test@email.com', password: 'password' })
     .then((result) => {
@@ -29,22 +33,112 @@ it('createUser cannot be called without being authenticated', () => {
     });
 });
 
-it('createUser cannot be called by non-admin', () => {
+test('createUser cannot be called by non-admin', () => {
+  jest.setTimeout(30000);
   const createUser = firebase.functions().httpsCallable('createUser');
   return firebase
     .auth()
     .signInWithEmailAndPassword('testuser@normaluser.com', 'normaluser')
     .then(() => {
-      createUser({ email: 'test@email.com', password: 'password' })
+      return createUser({ email: 'test@email.com', password: 'password' })
         .then((result) => {
           throw new Error("This shouldn't happen!");
         })
         .catch((err) => {
           expect(err.code).toEqual('failed-precondition');
-          expect(err.message).toEqual('The function must be called by an admin');
+          expect(err.message).toEqual('The function must be called by an admin.');
         });
     })
     .catch((err) => {
+      console.log(err);
       throw Error("This shouldn't happen!");
     });
 });
+
+// test('createUser works for admins', () => {
+//   const createUser = firebase.functions().httpsCallable('createUser');
+//   firebase
+//     .auth()
+//     .signInWithEmailAndPassword('testuser@isadmin.com', 'isadmin')
+//     .then(() => {
+//       createUser({ email: 'test@email.com', password: 'password' })
+//         .then((result) => result.data)
+//         .then((userRecord) => {
+//           // Check that the endpoint responded with the proper user
+//           expect(userRecord.email).toEqual('test@email.com');
+//           firebase
+//             .auth()
+//             .signInWithEmailAndPassword('test@email.com', 'password')
+//             .then(() => {
+//               // Check that we can sign in with this user
+//               const currentUser = firebase.auth().currentUser;
+//               if (currentUser === null) {
+//                 throw new Error("This shouldn't happen!");
+//               }
+//               expect(currentUser.email).toEqual('test@email.com');
+//               // Check that we have a corresponding user in our users collection whose uuid field has been filled out appropriately
+//               db.collection('users')
+//                 .doc('test@email.com')
+//                 .get()
+//                 .then((userSnapshot) => userSnapshot.data())
+//                 .then((user) => {
+//                   if (user !== undefined) {
+//                     throw new Error('No such document!');
+//                     // expect(user.uuid).toEqual(currentUser.uid);
+//                   } else {
+//                     throw new Error('No such document!');
+//                   }
+//                 })
+//                 .catch(function (error) {
+//                   throw new Error('Error getting document:' + error);
+//                 });
+//             })
+//             .catch((err) => {
+//               throw new Error("This shouldn't happen!");
+//             });
+//         })
+//         .catch((err) => {
+//           throw new Error("This shouldn't happen!");
+//         });
+//     })
+//     .catch((err) => {
+//       throw Error("This shouldn't happen!");
+//     });
+// });
+
+// const createUser = firebase.functions().httpsCallable('createUser');
+// var er1;
+// var er2;
+// var res;
+// firebase
+//   .auth()
+//   .signInWithEmailAndPassword('testuser@isadmin.com', 'isadmin')
+//   .then(() => {
+//     createUser({ email: 'test@email.com', password: 'password' })
+//       .then((result) => {
+//         res = result;
+//       })
+//       .catch((err) => {
+//         er1 = err;
+//       });
+//   })
+//   .catch((err) => {
+//     er2 = err;
+//   });
+
+// firebase
+//   .auth()
+//   .signInWithEmailAndPassword('testuser@normaluser.com', 'normaluser')
+//   .then(() => {
+//     createUser({ email: 'test@email.com', password: 'password' })
+//       .then((result) => {
+//         throw new Error("This shouldn't happen!");
+//       })
+//       .catch((err) => {
+//         expect(err.code).toEqual('failed-precondition');
+//         expect(err.message).toEqual('The function must be called by an admin');
+//       });
+//   })
+//   .catch((err) => {
+//     throw Error("This shouldn't happen!");
+//   });
