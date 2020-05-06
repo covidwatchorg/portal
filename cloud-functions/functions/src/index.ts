300f243efaae6e7d909ea89220bc9cc620910b92
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2020-05-06 13:47:41 -0700

    created a bunch of utility functions and a check for whether the organization exists

--- cloud-functions/functions/src/index.ts
+++ cloud-functions/functions/src/index.ts
@@ -21,6 +21,57 @@ function isCreateUserRequestProperlyFormatted(newUser: any): boolean {
   return newUser.email !== undefined && newUser.password !== undefined && newUser.organizationID !== undefined;
 }
 
+function doesOrganizationExist(organizationID: string): Promise<boolean> {
+  return db
+    .collection('organizations')
+    .doc(organizationID)
+    .get()
+    .then((document) => {
+      return document.exists;
+    })
+    .catch((err) => {
+      console.error(err);
+      return false;
+    });
+}
+
+// Deletes user from users table, if an entry exists
+function usersCollectionDeleteUser(email: string) {
+  db.collection('users')
+    .doc(email)
+    .get()
+    .then((user) => {
+      if (user.exists) {
+        user.ref.delete().catch((err) => {
+          console.error(err);
+        });
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+// Deletes user from firebase auth and from users table, if an entry exists
+function deleteUser(uid: string) {
+  auth
+    .getUser(uid)
+    .then((userRecord) => {
+      const email = userRecord.email;
+      auth
+        .deleteUser(uid)
+        .then(() => {
+          usersCollectionDeleteUser(email ? email : '');
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
 // Throw error if user is not authenticated
 function authGuard(context: functions.https.CallableContext): Promise<void> {
   return new Promise((resolve, reject) => {
@@ -56,45 +107,58 @@ export const createUser = functions.https.onCall((newUser, context) => {
             )
           );
         }
-        const newUserPrivileges = {
-          isAdmin: false,
-          isSuperAdmin: false,
-          organizationID: newUser.organizationID,
-        };
-        db.collection('users')
-          .doc(newUser.email)
-          .set(newUserPrivileges) /* Create new user in our Firestore record */
-          .then(() => {
-            // Create Firebase Auth record of the user
-            auth
-              .createUser({
-                email: newUser.email,
-                password: newUser.password,
-                emailVerified: false,
-                disabled: false,
-              })
-              .then((userRecord) => {
-                resolve(userRecord.toJSON());
-              })
-              .catch((err) => {
-                console.error(err);
-                if (err.errorInfo.code === 'auth/email-already-exists') {
-                  reject(new functions.https.HttpsError('already-exists', err.errorInfo.message));
-                } else {
-                  reject(new functions.https.HttpsError('internal', err.errorInfo.message));
-                }
-              });
+        doesOrganizationExist(newUser.organizationID)
+          .then((doesExist) => {
+            if (doesExist) {
+              const newUserPrivileges = {
+                isAdmin: false,
+                isSuperAdmin: false,
+                organizationID: newUser.organizationID,
+              };
+              db.collection('users')
+                .doc(newUser.email)
+                .set(newUserPrivileges) /* Create new user in our Firestore record */
+                .then(() => {
+                  // Create Firebase Auth record of the user
+                  auth
+                    .createUser({
+                      email: newUser.email,
+                      password: newUser.password,
+                      emailVerified: false,
+                      disabled: false,
+                    })
+                    .then((userRecord) => {
+                      resolve(userRecord.toJSON());
+                    })
+                    .catch((err) => {
+                      if (err.errorInfo.code === 'auth/email-already-exists') {
+                        reject(new functions.https.HttpsError('already-exists', err.errorInfo.message));
+                      } else {
+                        reject(new functions.https.HttpsError('internal', err.errorInfo.message));
+                      }
+                    });
+                })
+                .catch((err) => {
+                  reject(err);
+                });
+            } else {
+              reject(
+                new functions.https.HttpsError(
+                  'invalid-argument',
+                  'attempted to sign up user with an organization id that DNE:' + newUser.organizationID
+                )
+              );
+            }
           })
           .catch((err) => {
-            console.error(err);
-            reject(err);
+            reject(new functions.https.HttpsError('internal', err.errorInfo.message));
           });
       })
       .catch((err) => {
-        console.error(err);
         reject(err);
       });
   }).catch((err) => {
+    console.error(err);
     throw err;
   });
 });
@@ -112,70 +176,63 @@ export const onCreate = functions.auth.user().onCreate((firebaseAuthUser) => {
     .get()
     .then((covidWatchUser) => {
       if (covidWatchUser.exists) {
+        const covidWatchUserData = covidWatchUser.data()!;
         // Forced unwrapping warranted because data() only returns undefined if ref.exists is falsey
-        if (!isCovidWatchUserProperlyFormatted(covidWatchUser.data()!)) {
+        if (!isCovidWatchUserProperlyFormatted(covidWatchUserData)) {
           // delete from auth
           console.error(
             'Attempted to register new user, but corresponding entry in the users collection was not properly formatted'
           );
-          auth
-            .deleteUser(firebaseAuthUser.uid)
-            .then(() => {
-              console.log('Successfully deleted user');
-            })
-            .catch((err) => {
-              // TODO should send us an email to look into this
-              console.log('Error deleting user:', err);
-            });
-          // delete from users collection
-          covidWatchUser.ref.delete().catch((err) => {
-            console.error(err);
-          });
+          deleteUser(covidWatchUserData.uid);
         } else {
-          // User has been properly pre-registered in `users` collection, update the `users` entry with
-          // their automatically generated UID and set custom claims in their token
-          covidWatchUser.ref
-            .update({
-              uuid: firebaseAuthUser.uid,
-            })
-            .then(() => {
-              console.log('user ' + covidWatchUser.id + 'uuid updated to ' + firebaseAuthUser.uid);
-              auth
-                .setCustomUserClaims(firebaseAuthUser.uid, {
-                  // Forced unwrapping is warranted, because data integrity is checked above
-                  isSuperAdmin: covidWatchUser.data()!.isSuperAdmin,
-                  isAdmin: covidWatchUser.data()!.isAdmin,
-                  organizationID: covidWatchUser.data()!.organizationID,
-                })
-                .then(() => {
-                  console.log(
-                    'user ' + covidWatchUser.id + 'isSuperAdmin claim set to ' + covidWatchUser.data()?.isSuperAdmin
-                  );
-                  console.log('user ' + covidWatchUser.id + 'isAdmin claim set to ' + covidWatchUser.data()?.isAdmin);
-                  console.log(
-                    'user ' + covidWatchUser.id + 'organizationID claim set to ' + covidWatchUser.data()?.organizationID
-                  );
-                })
-                .catch((err) => {
-                  console.error(err);
-                });
+          doesOrganizationExist(covidWatchUserData.organizationID)
+            .then((doesExist) => {
+              if (doesExist) {
+                // User has been properly pre-registered in `users` collection, update the `users` entry with
+                // their automatically generated UID and set custom claims in their token
+                covidWatchUser.ref
+                  .update({
+                    uuid: firebaseAuthUser.uid,
+                  })
+                  .then(() => {
+                    console.log('user ' + covidWatchUser.id + 'uuid updated to ' + firebaseAuthUser.uid);
+                    auth
+                      .setCustomUserClaims(firebaseAuthUser.uid, {
+                        // Forced unwrapping is warranted, because data integrity is checked above
+                        isSuperAdmin: covidWatchUserData.isSuperAdmin,
+                        isAdmin: covidWatchUserData.isAdmin,
+                        organizationID: covidWatchUserData.organizationID,
+                      })
+                      .then(() => {
+                        console.log(
+                          'user ' + covidWatchUser.id + 'isSuperAdmin claim set to ' + covidWatchUserData.isSuperAdmin
+                        );
+                        console.log('user ' + covidWatchUser.id + 'isAdmin claim set to ' + covidWatchUserData.isAdmin);
+                        console.log(
+                          'user ' +
+                            covidWatchUser.id +
+                            'organizationID claim set to ' +
+                            covidWatchUserData.organizationID
+                        );
+                      })
+                      .catch((err) => {
+                        console.error(err);
+                      });
+                  })
+                  .catch((err) => {
+                    console.error(err);
+                  });
+              } else {
+                deleteUser(firebaseAuthUser.uid);
+              }
             })
             .catch((err) => {
               console.error(err);
-              throw err;
             });
         }
       } else {
         // User has not been properly pre-registered in `users` collection, delete this user from Firebase Auth
-        auth
-          .deleteUser(firebaseAuthUser.uid)
-          .then(() => {
-            console.log('Successfully deleted user');
-          })
-          .catch((err) => {
-            // TODO should send us an email to look into this
-            console.log('Error deleting user:', err);
-          });
+        deleteUser(firebaseAuthUser.uid);
       }
     })
     .catch((err) => {
