5331b1b0fef3c69ca45dd25398b964d1708935d7
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2020-05-08 21:15:30 -0700

    doing what I should have tried to do from the beginning, using the github ci key string

--- cloud-functions/functions/__tests__/auth.test.ts
+++ cloud-functions/functions/__tests__/auth.test.ts
@@ -36,26 +36,11 @@ firebase.initializeApp(firebaseConfig);
 // Initialize admin SDK
 const serviceAccount =
   process.env.NODE_ENV === 'ci'
-    ? {
-        type: 'service_account',
-        project_id: 'permission-portal-test',
-        private_key_id: 'c8874cba45031dd6df96dac0319b61c12ff1a72a',
-        private_key: process.env.private_key,
-        client_email: 'firebase-adminsdk-nqxd8@permission-portal-test.iam.gserviceaccount.com',
-        client_id: '106962391164160767637',
-        auth_uri: 'https://accounts.google.com/o/oauth2/auth',
-        token_uri: 'https://oauth2.googleapis.com/token',
-        auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
-        client_x509_cert_url:
-          'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-nqxd8%40permission-portal-test.iam.gserviceaccount.com',
-      }
+    ? process.env.FIREBASE_TOKEN
     : require('../../permission-portal-test-firebase-admin-key.json');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL:
-    process.env.NODE_ENV === 'development'
-      ? 'https://permission-portal-dev.firebaseio.com'
-      : 'https://permission-portal-test.firebaseio.com',
+  databaseURL: 'https://permission-portal-test.firebaseio.com',
 });
 
 // Initialize commonly used vars
