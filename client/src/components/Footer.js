55b0a9fd2423efa262cabcae8a804be680190e79
fordacious <fordyfor@hotmail.com>
2020-05-10 18:31:38 -0700

    Added logo to footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
-const Footer = () => {
+const constructClassName = (isBranded) => {
+    return ;
+};
+
+const Footer = (props) => {
   return (
-    <div className="footerContainer">
+    <div className={"footerContainer" + (props.branded ? " branded" : "")}>
       <p id="copyright">© 2020 Covid Watch. All rights reserved.</p>
       <a href="url">Privacy Policy</a>
       <a href="url">Terms of Use</a>
