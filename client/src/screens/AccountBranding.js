591aa74d6c8ece763eb4cafe15f3f2d832f17811
fordacious <fordyfor@hotmail.com>
2020-05-10 19:38:17 -0700

    cleaned up branding page but missing functionality

--- client/src/screens/AccountBranding.js
+++ client/src/screens/AccountBranding.js
@@ -1,25 +1,51 @@
 import React, { useState } from 'react';
 import "../../Styles/screens/branding.scss";
 
-const defaultDiagnosisText = `Next Steps:
+var defaultDiagnosisText = `Next Steps:
 - Please quarantine yourself
 -
 -
 `;
 
-const defaultExposureText = `Next Steps:
+var defaultExposureText = `Next Steps:
 - Please quarantine yourself
 - Do you need help with shelter, food etc.?
 -
 `;
 
+const getSavedDiagnosisText = () => {
+  return defaultDiagnosisText;
+};
+
+const getSavedExposureText = () => {
+  return defaultExposureText
+}
+
+// TODO this operation may fail, should return success confirmation
+const SaveAccountBrandingData = (diagnosisText, setDiagnosisText, exposureText, setExposureText) => {
+  console.log("TODO save account branding data");
+  console.log(diagnosisText);
+  console.log(exposureText);
+
+  defaultDiagnosisText = diagnosisText;
+  setDiagnosisText(defaultDiagnosisText);
+
+  defaultExposureText = exposureText;
+  setExposureText(defaultExposureText);
+}
+
 const AccountBranding = () => {
-  const [diagnosisText, setDiagnosisText] = useState(defaultDiagnosisText);
-  const [exposureText, setExposureText] = useState(defaultExposureText);
-  
+  const savedDiagnosisText = getSavedDiagnosisText();
+  const savedExposureText = getSavedExposureText();
+
+  const [diagnosisText, setDiagnosisText] = useState(savedDiagnosisText);
+  const [exposureText, setExposureText] = useState(savedExposureText);
+
+  const changedSinceLastSave = savedDiagnosisText !== diagnosisText || savedExposureText !== exposureText;
+
   return (
     <div className="module-container">
-      <h1>Account Branding</h1>
+      <h1 className="branding-header">Account Branding</h1>
       <div className="branding-container">
         <div className="branding-section">
           <h2 className="section-heading">Share Positive Diagnosis</h2>
@@ -32,12 +58,6 @@ const AccountBranding = () => {
             value={diagnosisText}
             onChange={e => setDiagnosisText(e.target.value)}
           />
-          {
-            (diagnosisText !== defaultDiagnosisText) &&
-            <div className="save-button" onClick={() => {}}>
-              Save Changes
-            </div>
-          }
         </div>
         <div className="branding-section">
           <h2 className="section-heading">Possible Exposure</h2>
@@ -50,12 +70,6 @@ const AccountBranding = () => {
             value={exposureText}
             onChange={e => setExposureText(e.target.value)}
           />
-          {
-            (exposureText !== defaultExposureText) &&
-            <div className="save-button" onClick={() => {}}>
-              Save Changes
-            </div>
-          }
         </div>
         <div className="branding-section">
           <h2 className="section-heading">Other Branding and Customization</h2>
@@ -66,6 +80,14 @@ const AccountBranding = () => {
             Contact Us
           </div>
         </div>
+      </div>
+      <div className="save-button-container">
+        {
+          changedSinceLastSave &&
+          <div className="save-button" onClick={() => SaveAccountBrandingData(diagnosisText, setDiagnosisText, exposureText, setExposureText)}>
+            Save Changes
+          </div>
+        }
       </div>
     </div>
   );
