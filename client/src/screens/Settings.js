32225249e375974b257392fb2bb34a95d0ddbf1d
kimdrosa <kimdeeerosa@gmail.com>
2020-05-11 13:00:09 -0700

    UI fixes

--- client/src/screens/Settings.js
+++ client/src/screens/Settings.js
@@ -18,21 +18,51 @@ const useStyles = makeStyles({
 
 const inputStyles = makeStyles({
   root : {
+    fontFamily: 'Montserrat',
     boxShadow: 'inset 0px 2px 10px rgba(0, 0, 0, 0.2)',
     borderRadius: 7,
     border: '2px solid #BDBDBD',
+    paddingLeft: 10,
     width: '75%',
     height: 30,
-    marginTop: 5,
-    marginBottom: 20
-
+    fontSize: 18,
+    marginTop: 10,
+    marginBottom: 40
+      
   }
+})
+
+const primaryButtonStyles = makeStyles({
+  root : {
+  backgroundColor:'#2C58B1', 
+  color:'white',
+  width:'75%', 
+  fontSize:'18px',
+  padding:'5px',
+  borderRadius:'7px',
+  height: 40,
+  marginTop: 30
+  }
+
+})
+const secondaryButtonStyles = makeStyles({
+  root : { 
+  color:'#2C58B1', 
+  width:'195px', 
+  height: 40,
+  fontSize:'18px', 
+  padding:'5px',
+  border: '2px solid #BDBDBD',
+  borderRadius:'7px'
+}
 })
 
 const Settings = () => {
 
   const classes = useStyles();
   const input = inputStyles();
+  const secondaryButton = secondaryButtonStyles();
+  const primaryButton = primaryButtonStyles()
  
   const settingsForm = () => (
     <Fragment>
@@ -43,55 +73,51 @@ const Settings = () => {
         <Grid item xs={4}>
           <Grid container spacing={2} direction='column'>
               Profile Photo
-              <div style={{marginTop:'20px', height:'217px', width:'212px', backgroundColor:'#E0E0E0', border: '2px dashed #828282'}}>
+              <div style={{marginTop:'20px', height:'200px', width:'195px', backgroundColor:'#E0E0E0', border: '2px dashed #828282'}}>
               </div>
-              <text style={{fontSize:'12px', color:'#585858'}}>
+              <text style={{marginTop:'15px', fontSize:'12px', color:'#585858'}}>
                 Accepted file types: jpg or png
               </text>
-              <text style={{fontSize:'12px', color:'#585858'}}>
+              <text style={{ marginBottom:'15px', fontSize:'12px', color:'#585858'}}>
                 Maximum file size: __ MB
               </text>
-              <Button onClick={console.log('button clicked')} className={classes.root} variant='outlined' style={{ color:'#2C58B1', width:'75%', fontSize:'smaller', padding:'5px'}}>
-                Change Image
-              </Button>
+              <button type='submit' className={secondaryButton.root}>Change Image</button>
           </Grid>
         </Grid>
 
         <Grid item xs={4}>
           <Grid container spacing={2} direction='column'>
-              <label>
+              <label for='prefix'>
                 Prefix 
               </label>
-              <TextField className={input.root} label="-" variant="outlined"/>
-              <label>
+              <input type="text" id='prefix' name='prefix' className={input.root}></input>
+              <label for='firstName'>
                 First Name <text style={{color:'red'}}>*</text>
               </label>
-              <TextField required className={input.root} label="" variant="outlined" />
-              <label>
+              <input type="text" id='firstName' name='firstName' required className={input.root}></input>
+              <label for='email'>
                 Email Address <text style={{color:'red'}}>*</text>
               </label>
-              <TextField required className={input.root} label="" variant="outlined" />
-              <Button onClick={console.log('button clicked')} className={classes.root} variant='contained' style={{backgroundColor:'#2C58B1', color:'white', width:'75%', fontSize:'smaller', padding:'5px'}}>
-                Save Changes
-              </Button>
+              <input type="text" id='email' name='email' required className={input.root}></input>
+              <button type='submit' className={primaryButton.root}>Save Changes</button>
           </Grid>
         </Grid>
 
         <Grid item xs={4}>
           <Grid container spacing={2} direction='column'>
-            <label>
+            <label for='role'>
               Role <text style={{color:'red'}}>*</text>
             </label>
-            <TextField required  className={input.root}  label="" variant="outlined" style={{backgroundColor: '#E0E0E0'}} />
-            <label>
-              Last Name <text style={{color:'red'}}>*</text>
+            <input type="text" id='role' name='role' value='Administrator' required className={input.root} style={{backgroundColor:'#E0E0E0'}}></input>
+            <label for='lastName'>
+              Last Name <text required style={{color:'red'}}>*</text>
             </label>
-            <TextField required className={input.root} label="" variant="outlined" />
-            <label>
+            <input type="text" id='lastName' name='lastName' required className={input.root}></input>
+            <label for='password'>
               Password <text style={{color:'red'}}>*</text>
             </label>
-            <TextField required className={input.root}  label="" type="password" variant="outlined" style={{backgroundColor: '#E0E0E0'}}/>
-            <a href='' style={{fontSize:'12px', textAlign:'right', color:'#2C58B1', fontStyle:'underline'}}>Change Password</a>
+            <input required className={input.root}  id='password' name='password' type="password"  value='example' style={{backgroundColor: '#E0E0E0'}}/>
+            <a href='' style={{fontSize:'12px', textAlign:'right', marginRight:'50px', color:'#2C58B1', fontStyle:'underline'}}>Change Password</a>
           </Grid>
         </Grid>
     
