fcaf3735deefaf3352bacf7b605346f0591ec22a
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2020-05-11 13:56:41 -0700

    working now iff user isAdmin

--- client/src/util/AdminRoute.js
+++ client/src/util/AdminRoute.js
@@ -3,7 +3,7 @@ import { Route, Redirect } from 'react-router-dom'
 import { auth } from './auth'
 
 const AdminRoute = (props) => {
-  return <Route path={props.path}>{auth.checkIfAdmin() ? props.children : <Redirect to="/" />}</Route>
+  return <Route path={props.path}>{auth.isAdmin ? props.children : <Redirect to="/" />}</Route>
 }
 
 export default AdminRoute
