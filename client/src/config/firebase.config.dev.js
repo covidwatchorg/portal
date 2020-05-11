15da004811f47a0f52de735b8c01c74f0b242480
Prabhu Inbarajan <inbarajan.prabhu@gmail.com>
2020-05-10 20:15:33 -0700

    empty dev config

---
+++ client/src/config/firebase.config.dev.js
@@ -0,0 +1,13 @@
+
+module.exports =  {
+  apiKey: "from firebase console",
+  authDomain: "from firebase console",
+  databaseURL: "from firebase console",
+  projectId: "from firebase console",
+  storageBucket: "from firebase console",
+  messagingSenderId: "from firebase console",
+  appId: "from firebase console"
+};
+
+
+//export firebaseConfig;
