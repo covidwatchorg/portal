a72e175b0b924af51aa256fa0224c56dc60d7f7d
Evelyn Fitzgerald <acf67@cornell.edu>
2020-05-20 14:12:43 -0400

    Modal: Replace 'X' with close icon

--- permission-portal-frontend/client/src/components/Modal.js
+++ permission-portal-frontend/client/src/components/Modal.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import close from '../../assets/close.svg'
 import Snackbar from "@material-ui/core/Snackbar";
 import SnackbarContent from '@material-ui/core/SnackbarContent';
 
@@ -7,7 +8,7 @@ const Modal = (props) => {
   return (
     <div className={"modal-background" + (props.hidden === true ? ' hidden' : '')}>
       <div className={"add-member-modal-container"}>
-        <span className="close-btn" onClick={props.onClose}>X</span>
+        <span className="close-btn" onClick={props.onClose}><img src={close} /></span>
           {props.children}
       </div>
     </div>
