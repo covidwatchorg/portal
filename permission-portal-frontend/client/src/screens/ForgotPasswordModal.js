aa1f0b7ddd5ff307e0b3a66a89ec139d0e624e0b
Nikhil Kumar <nikhilfaips@gmail.com>
2020-05-22 11:38:00 -0400

    Added failure message, minor style update

--- permission-portal-frontend/client/src/screens/ForgotPasswordModal.js
+++ permission-portal-frontend/client/src/screens/ForgotPasswordModal.js
@@ -6,7 +6,7 @@ import { firebase } from '../components/Firebase'
 class ForgotPasswordModal extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { emailPrompt: true, email: '', validEmail: true }
+    this.state = { emailPrompt: true, email: '', validEmail: true, isSuccess: false }
     this.onSubmit = this.onSubmit.bind(this)
     this.onClose = this.onClose.bind(this)
     this.handleChange = this.handleChange.bind(this)
@@ -20,10 +20,14 @@ class ForgotPasswordModal extends React.Component {
       return
     }
 
-    await firebase.sendPasswordResetEmail(this.state.email)
+    try {
+      await firebase.sendPasswordResetEmail(this.state.email)
 
-    // Show confirmation screen
-    this.setState({ emailPrompt: false, email: '', validEmail: true })
+      // Show confirmation screen
+      this.setState({ isSuccess: true, emailPrompt: false, email: '', validEmail: true })
+    } catch (err) {
+      this.setState({ isSuccess: false, emailPrompt: false, email: '', validEmail: true })
+    }
   }
 
   handleChange(event) {
@@ -59,9 +63,13 @@ class ForgotPasswordModal extends React.Component {
           <Modal hidden={this.props.hidden} onClose={this.onClose} containerClass="recover-password-modal-container">
             <h2>Recover Password</h2>
             <p>
-              {' '}
-              A password recovery link has been sent to the email address associated with your account. Please click the
-              link in the email to reset your password.{' '}
+              {
+                this.state.isSuccess ?
+              ' A password recovery link has been sent to the email address associated with your account. Please click the' +
+              ' link in the email to reset your password. ' :
+              ' Failed to send password recovery link. Please verify that the entered email address is associated with your' +
+              ' account and try again. '
+              }
             </p>
           </Modal>
         </div>
