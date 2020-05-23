b5f7a3638ec65676092a8cca530b678f25ebef2f
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2020-05-23 12:17:56 -0700

    added concerned citizen disclaimer

--- config/firebase.config.prod.js
+++ config/firebase.config.prod.js
@@ -1,5 +1,9 @@
-// get this from Firebase console, Project Settings -> General -> Web Apps -> Firebase SDK Snippet
-
+// Concerned Citizen,
+// We know what you're asking yourself: "Did these brain dead Covid Watch devs just check their API keys in to
+// an open source repository?". The answer is of course, yes we did. But don't worry, these are not "API keys"
+// in the normal sense of the word. They should rather be thought of as configuration URLs that tell our app
+// which Firebase infrastructure it should talk to. Firebase handles security in other ways, see the following
+// Stack Overflow link for more info: https://stackoverflow.com/questions/37482366/is-it-safe-to-expose-firebase-apikey-to-the-public
 module.exports = {
   apiKey: "AIzaSyCnaCYo8H_EfJNAbQkEQJeBypqnAHvtCNs",
   authDomain: "permission-portal.firebaseapp.com",
