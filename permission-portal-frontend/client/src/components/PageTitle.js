2ac7f8687ca34c419a1579cb48937a708a25d8e1
Evelyn Fitzgerald <acf67@cornell.edu>
2020-05-24 11:49:49 -0400

    Easier way to join page title parts together

--- permission-portal-frontend/client/src/components/PageTitle.js
+++ permission-portal-frontend/client/src/components/PageTitle.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import DocumentTitle from 'react-document-title'
+import { observer } from 'mobx-react'
+import { withStore } from '../store'
 
-const PageTitle = (props) => {
-  return (
-    <DocumentTitle
-      title={
-        props.title ? props.title + ' - Covid Watch Community Tracing Portal' : 'Covid Watch Community Tracing Portal'
-      }
-    >
-      {props.children}
-    </DocumentTitle>
-  )
-}
+const PageTitleBase = observer((props) => {
+  let title = [props.title, props.store.organization.name, 'Covid Watch Community Tracing Portal']
+    .filter(Boolean)
+    .join(' - ')
+
+  return <DocumentTitle title={title}>{props.children}</DocumentTitle>
+})
+
+const PageTitle = withStore(PageTitleBase)
 
 export default PageTitle
