3a432964b5837edb60da2404ffa456f4cea7d1b4
Lainey Neild <laineyneild@Laineys-MacBook-Pro.local>
2020-06-05 15:32:30 -0400

    Confirm role change

--- permission-portal-frontend/client/src/screens/ManageTeams.js
+++ permission-portal-frontend/client/src/screens/ManageTeams.js
@@ -86,6 +86,18 @@ const ManageTeamsBase = observer((props) => {
     setShowDeleteUserModal(false)
   }
 
+  const handleRoleChange = (e, isAdmin, firstName, lastName, email) => {
+    if (
+      confirm(
+        `Are you sure you want swicth ${firstName} ${lastName} from ${
+          isAdmin ? ROLES.ADMIN_LABEL : ROLES.NON_ADMIN_LABEL
+        } to ${isAdmin ? ROLES.NON_ADMIN_LABEL : ROLES.ADMIN_LABEL}`
+      )
+    ) {
+      props.store.updateUserByEmail(email, { isAdmin: e.target.value == ROLES.ADMIN_LABEL })
+    }
+  }
+
   const resetPassword = async (e, email) => {
     e.preventDefault()
     try {
@@ -139,9 +151,7 @@ const ManageTeamsBase = observer((props) => {
                 <td style={{ padding: 0 }}>
                   <RoleSelector
                     memberIndex={index}
-                    onChange={(e) => {
-                      props.store.updateUserByEmail(data.email, { isAdmin: e.target.value == ROLES.ADMIN_LABEL })
-                    }}
+                    onChange={(e) => handleRoleChange(e, data.isAdmin, data.firstName, data.lastName, data.email)}
                     ariaLabelledBy="role-header"
                   />
                 </td>
