796d154d77e9c8b4e611c0d88652112cbb9da006
Colby Morrison <colbyamorrison@gmail.com>
2020-06-11 22:16:39 -0700

    New member email style

--- cloud-functions/functions/src/index.ts
+++ cloud-functions/functions/src/index.ts
@@ -186,14 +186,15 @@ function sendNewUserEmail(email: string, password: string, firstName: string, la
     from: 'welcome@covid-watch.org',
     subject: 'Welcome to the Covid Watch Permission Portal',
     html: `
-    <p>${firstName} ${lastName},</p>
-    <p>You are receiving this email because you were added as a new member of Covid Watch by an Account Administrator.</p>
-    <p><em>Your user name:</em> ${email}<br />  <em>Your password:</em> ${password}</p>
-    <p>Please click the following link or copy-paste it in your browser to sign in to your new account.</p>
-    <p><a href=${functions.config().client.url}>Sign In</a></p>
-    <p>If you recieved this message in error, you can safely ignore it.</p>
-    <p>You can reply to this message, or email support@covid-watch.org if you have any questions.</p>
-    <p>Thank you,<br />Covid Watch Team</p>`,
+    <!DOCTYPE html>
+    <p style="font-family: Montserrat;font-size:18px;color: #585858;">Colby Morrison,</p>
+    <p style="font-family: Montserrat;font-size:18px;color: #585858;">You are receiving this email because you were added as a new member of Covid Watch by the Account Administrator.</p>
+    <p style="font-family: Montserrat;font-size:18px;color: #585858;"><b>Your user name:</b> ${email}<br />  <b>Your temporary password:</b> ${password}</p>
+    <p style="font-family: Montserrat;font-size:18px;color: #585858;">Please click the following link or copy and paste it into your browser to sign in to your new account:</p>
+    <p style="font-family: Montserrat;font-size:18px;color: #585858;"><a href=${functions.config().client.url}>Sign In</a></p>
+    <p style="font-family: Montserrat;font-size:18px;color: #585858;">If you recieved this message in error, you can safely ignore it.</p>
+    <p style="font-family: Montserrat;font-size:18px;color: #585858;">You can reply to this message, or email support@covid-watch.org if you have any questions.</p>
+    <p style="font-family: Montserrat;font-size:18px;color: #585858;">Thank you,<br />Covid Watch Team</p> `,
   };
   sgMail
     .send(msg)
