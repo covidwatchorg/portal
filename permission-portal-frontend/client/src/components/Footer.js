da05e08e001fddc4059053a14c73120b6d16e5e9
Jesse Colligan <jesse.m.colligan@gmail.com>
2020-06-16 22:03:54 -0700

    adjustment to footer and navbar for 404 page customization

--- permission-portal-frontend/client/src/components/Footer.js
+++ permission-portal-frontend/client/src/components/Footer.js
@@ -4,27 +4,43 @@ import { useLocation } from 'react-router'
 const Footer = () => {
   const cwImg = require('../../assets/powered-by-cw.svg')
   let location = useLocation()
+  let style = {};
+  let text = (
+    <div className="footer">
+      <p id="copyright">© 2020 Covid Watch. All rights reserved.</p>
+      <a href="url">Privacy Policy</a>
+      <a href="url">Terms of Use</a>
+    </div>
+  )
+  
+  if (location.pathname === '/') {
+    // style remains empty object {}
+  } else if (location.pathname === '/404') {
+    // style remains empty object {}
+    text = (
+      <div></div>
+    );
+  } else {
+    style = {
+      minHeight: '100%',
+      position: 'relative',
+      background: 'url(' + cwImg + ')',
+      backgroundRepeat: 'no-repeat',
+      backgroundPositionX: '115px',
+      backgroundPositionY: '36px',
+    }
+  }
 
   return (
     <div id="footer">
       <div
         className="footerContainer"
-        style={
-          location.pathname !== '/'
-            ? {
-                minHeight: '100%',
-                position: 'relative',
-                background: 'url(' + cwImg + ')',
-                backgroundRepeat: 'no-repeat',
-                backgroundPositionX: '115px',
-                backgroundPositionY: '36px',
-              }
-            : {}
-        }
+        style={style}
       >
-        <p id="copyright">© 2020 Covid Watch. All rights reserved.</p>
+        {text}
+        {/* <p id="copyright">© 2020 Covid Watch. All rights reserved.</p>
         <a href="url">Privacy Policy</a>
-        <a href="url">Terms of Use</a>
+        <a href="url">Terms of Use</a> */}
       </div>
     </div>
   )
