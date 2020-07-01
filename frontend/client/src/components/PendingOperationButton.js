f0b352d15c396ff5dd8c70306e21f0a9f7310a17
Evelyn Fitzgerald <acf67@cornell.edu>
2020-07-01 18:36:09 -0400

    change progress-container to class

--- frontend/client/src/components/PendingOperationButton.js
+++ frontend/client/src/components/PendingOperationButton.js
@@ -36,7 +36,7 @@ const PendingOperationButton = (props) => {
     )
   } else {
     return (
-      <div id="progress-container">
+      <div className="progress-container">
         <CircularProgress />
       </div>
     )
