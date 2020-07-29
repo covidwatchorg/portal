c64f30d74465100a85338d99ccb907425246f236
Jesse Colligan <jesse.m.colligan@gmail.com>
2020-07-28 21:22:39 -0700

    adjust NavBar to show covid watch logo before logging in or before logo is set

--- frontend/client/src/components/NavBar.js
+++ frontend/client/src/components/NavBar.js
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles'
 import IconButton from '@material-ui/core/IconButton'
 import MenuItem from '@material-ui/core/MenuItem'
 import Menu from '@material-ui/core/Menu'
-import UofA from '../../assets/uofa-logo.svg'
+import cwLogo from '../../assets/covid-watch-logo-blue.svg'
 import profile from '../../assets/placeholder/profile.png'
 import * as ROLES from '../constants/roles'
 import { withStore } from '../store'
@@ -127,7 +127,7 @@ const NavBarBase = observer((props) => {
   return (
     <div className="navbarContainer">
       <Link to="/code_validations">
-        <img src={UofA} id="orgLogo" alt={props.store.data.organization.name || 'University of Arizona'} />
+        <img src={props.store.data.organization.logoBlob ? props.store.data.organization.logoBlob : cwLogo} id="orgLogo" alt={props.store.data.organization.name} />
       </Link>
       {props.store.data.user.firstName ? LoggedInIcons : <div id="logged-in-icons-container" />}
     </div>
