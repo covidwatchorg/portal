24bfeb4a581b718e11b46f883de1a2b2190e5d4f
leon-i <isakleon97@gmail.com>
2020-07-29 18:07:02 -0400

    Formatting

--- frontend/client/src/components/Footer.js
+++ frontend/client/src/components/Footer.js
@@ -11,7 +11,11 @@ const Footer = () => {
         </div>
         <div className="xs-text" id="footer-text">
           <p id="copyright">© 2020 Covid Watch. All rights reserved.</p>
-          <a target="_blank" href="https://docs.google.com/document/d/1pcnFidqiebgQpxxVoi0VCoiRsSndCl5VF_7kZMtnZOA/edit?usp=sharing" rel="noreferrer">
+          <a
+            target="_blank"
+            href="https://docs.google.com/document/d/1pcnFidqiebgQpxxVoi0VCoiRsSndCl5VF_7kZMtnZOA/edit?usp=sharing"
+            rel="noreferrer"
+          >
             Privacy Policy
           </a>
           {/* <a target="_blank" rel="noreferrer">
