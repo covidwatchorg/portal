c1126cc24ae18f71869bc87cabbb7cda35a4aa81
leon-i <isakleon97@gmail.com>
2020-07-31 15:32:12 -0400

    Removed useStyles, replaced with scss

--- frontend/client/src/util/sample.js
+++ frontend/client/src/util/sample.js
@@ -10,7 +10,6 @@ import Grid from '@material-ui/core/Grid'
 import Box from '@material-ui/core/Box'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 import Typography from '@material-ui/core/Typography'
-import { makeStyles } from '@material-ui/core/styles'
 import Container from '@material-ui/core/Container'
 
 function Copyright() {
@@ -26,40 +25,19 @@ function Copyright() {
   )
 }
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}))
-
 export default function SignIn() {
-  const classes = useStyles()
 
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component="main" maxWidth="xs" id='sample-main'>
       <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
+      <div className='sample-paper'>
+        <Avatar className='sample-avatar'>
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        <form className={classes.form} noValidate>
+        <form className='form' noValidate>
           <TextField
             variant="outlined"
             margin="normal"
@@ -83,7 +61,7 @@ export default function SignIn() {
             autoComplete="current-password"
           />
           <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
-          <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
+          <Button type="submit" fullWidth variant="contained" color="primary" className='sample-submit-btn'>
             Sign In
           </Button>
           <Grid container>
