e9f1eb492b700cea1ef45df15663ebdcd64a59ba
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2020-08-12 12:46:26 -0600

    disabling react dev tools for staging and production

--- frontend/client/index.js
+++ frontend/client/index.js
@@ -3,10 +3,16 @@ import React from 'react'
 import App from './App'
 import * as Sentry from '@sentry/react'
 import './styles/index.scss'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
 Sentry.init({
   dsn: 'https://13821d959c3a4f10944bb8ef579d034d@o410040.ingest.sentry.io/5283616',
   environment: window.env,
 })
+
+// Disable react dev tools to make it difficult for hackers to access the store and explore protected routes
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+  disableReactDevTools()
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
