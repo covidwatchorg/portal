e52dedbb10b6f12cfaff66dcb870bd69a8366e36
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2020-08-14 14:54:13 -0600

    checks that component is mounted before setting component state to solve memory leak bug

--- frontend/client/src/components/PendingOperationButton.js
+++ frontend/client/src/components/PendingOperationButton.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import CircularProgress from '@material-ui/core/CircularProgress'
 
 /**
@@ -18,20 +18,39 @@ const PendingOperationButton = (props) => {
   const operation = props.operation || (() => {})
   const disabled = props.disabled || false
 
-  const startOperation = async () => {
-    setIsOperationPending(true)
+  // Flag to keep track of whether component is mounted. We check this flag in runOperation before setting the button's state,
+  // in order to prevent cases where we mistakenly try to update the state when the button is no longer mounted, which was
+  // happening relatively frequently in i.e. automatic-modal-closes and triggering the following warning:
+  // "Warning: Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application.
+  // To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function."
+  var componentIsMounted = false
+  useEffect(() => {
+    componentIsMounted = true
+    return function cleanup() {
+      componentIsMounted = false
+    }
+  }, [])
+
+  const runOperation = async () => {
+    if (componentIsMounted) {
+      setIsOperationPending(true)
+    }
     try {
       await operation()
-      setIsOperationPending(false)
+      if (componentIsMounted) {
+        setIsOperationPending(false)
+      }
     } catch {
-      setIsOperationPending(false)
+      if (componentIsMounted) {
+        setIsOperationPending(false)
+      }
     }
   }
 
   if (!isOperationPending) {
     return (
       <div className="progress-container">
-        <button className={className} disabled={disabled} style={style} onClick={startOperation}>
+        <button className={className} disabled={disabled} style={style} onClick={runOperation}>
           {props.children}
         </button>
       </div>
