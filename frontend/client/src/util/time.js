b15fc01426db18585af80c858c855a870ce796c7
Jesse <jesse.m.colligan@gmail.com>
2020-08-16 12:19:09 -0700

    solve code expire hour time being 0 when it should be 12 (#513)
    * solve code expire hour time being 0 when it should be 12
    
    * remove double comment out
    
    * further adjustments
    
    * resolve undefined AM/PM for some AM times
    
    * set first conditional to hours < 24 to capture when hours is 23
    
    * figured out how to do with builtins to simplify function
    
    * updated href to www.* since without it we were getting a 301 which was failing the test
    
    Co-authored-by: Isaiah Becker-Mayer <ibeckermayer@gmail.com>

--- frontend/client/src/util/time.js
+++ frontend/client/src/util/time.js
@@ -15,23 +15,7 @@ export const getOneHourAheadDisplayString = () => {
   // declaring new Date() here instead of passing in the code generation time stamp as they should be within 60 seconds of each other and second precision here is not necessary
   const now = new Date()
   const ahead = new Date(now.setHours(now.getHours() + 1))
-  let hours = ahead.getHours()
-  let amPM = 'AM'
-
-  if (hours > 12) {
-    hours = hours - 12
-    amPM = 'PM'
-  }
-
-  hours = hours.toString()
-  let mins = ahead.getMinutes().toString()
-
-  if (mins.length === 1) {
-    mins = '0' + mins
-  }
-  const timezone = getDefaultTimezoneString()
-
-  return hours + ':' + mins + ' ' + amPM + ' ' + timezone
+  return `${ahead.toLocaleTimeString([], { hour: 'numeric', minute: 'numeric' })} ${getDefaultTimezoneString()}`
 }
 
 // getFourteenDaysAgo generates a string formatted date fourteen days ago from present day
