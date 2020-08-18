840abdb1a9e76b1b9014e9d698c12a853a31fc77
Isaiah Becker-Mayer <ibeckermayer@gmail.com>
2020-08-18 12:42:56 -0600

    formatting and removing junk

--- cloud-functions/functions/config/functions.config.staging.js
+++ cloud-functions/functions/config/functions.config.staging.js
@@ -1,12 +1,12 @@
 var config = {
   client: {
-    url: "https://staging-portal.covidwatch.org/",
+    url: 'https://staging-portal.covidwatch.org/',
   },
   sendgrid: {
     key: process.env.SENDGRID_API_KEY,
   },
   verification_server: {
-    url: "https://dev.adminapi.verification.covidwatch.org",
+    url: 'https://dev.adminapi.verification.covidwatch.org',
     key: process.env.VERIFICATION_SERVER_API_KEY,
   },
 };
