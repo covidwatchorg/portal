39aef99fd70eac3282324e17d1f697d013d7239f
Colby Morrison <colbyamorrison@gmail.com>
2020-08-17 20:57:56 -0700

    test

--- config/functions.config.dev.js
+++ config/functions.config.dev.js
@@ -6,7 +6,8 @@ var config = {
     key: process.env.SENDGRID_API_KEY,
   },
   verification_server: {
-      url: "https://dev.adminapi.verification.covidwatch.org",
+    // Test comment remove me!
+    url: "https://dev.adminapi.verification.covidwatch.org",
     key: process.env.VERIFICATION_SERVER_API_KEY,
   },
 };
