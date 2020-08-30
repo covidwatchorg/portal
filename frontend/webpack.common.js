d6af82e5699c448e4fd172a1a64a003aff41ccdd
Colby Morrison <colbyamorrison@gmail.com>
2020-08-30 00:00:59 -0700

    ignore

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -14,7 +14,7 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?/,
-        exclude: /node_modules/,
+        exclude: /node_modules\/(?!(@fvilers)\/).*/,
         use: {
           loader: 'babel-loader',
         },
